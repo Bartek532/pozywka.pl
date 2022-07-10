162a21452b8f7eb5d2b8db4e0e53fb6d1d5d330e
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2022-07-10 20:06:48 +0200

    feat: add Layout component

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,89 @@
+import { memo, useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import styles from "./Layout.module.scss";
+import { Navbar } from "components/Navbar/Navbar";
+import { Footer } from "components/Footer/Footer";
+//import { CookiesPopup } from "components/CookiesPopup/CookiesPopup";
+import Logo from "public/svg/logo.svg";
+import clsx from "clsx";
+import { NextSeo } from "next-seo";
+import Link from "next/link";
+import type { YoastHead } from "types";
+//import { NavControls } from "components/Navbar/NavControls/NavControls";
+
+import { titleTemplate as defaultTitleTemplate } from "pages/_app";
+
+type LayoutProps = {
+  readonly children: React.ReactNode;
+  readonly title?: string;
+  readonly subtitle?: string;
+  readonly head?: YoastHead;
+  readonly titleTemplate?: string;
+};
+
+export const Layout = memo<LayoutProps>(({ children, title, subtitle, head, titleTemplate = defaultTitleTemplate }) => {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+  const router = useRouter();
+
+  return (
+    <div className={styles.content}>
+      {/*<CookiesPopup />*/}
+      <header className={styles.header}>
+        <Link href="/">
+          <a>
+            <Logo className={styles.logo} />
+            <span className="sr-only">strona główna</span>
+          </a>
+        </Link>
+        <button
+          onClick={() => setIsHamburgerOpen((val) => !val)}
+          className={clsx(styles.hamburger, {
+            [styles.hamburgerOpen]: isHamburgerOpen,
+          })}
+          title={`${isHamburgerOpen ? "Zamknij" : "Otwórz"} menu`}
+          aria-haspopup="true"
+          aria-controls="navigation"
+          aria-expanded="false"
+        >
+          <span className="sr-only">{isHamburgerOpen ? "Zamknij" : "Otwórz"} menu</span>
+        </button>
+      </header>
+
+      {title ? <h1 className={styles.title}>{title}</h1> : null}
+      {subtitle ? <h2 className={styles.subtitle} dangerouslySetInnerHTML={{ __html: subtitle }}></h2> : null}
+      {children}
+
+      <Footer />
+
+      <NextSeo
+        title={title ? titleTemplate.replace("%s", title) : titleTemplate.slice(4)}
+        description={head?.og_description}
+        openGraph={{
+          title: title ? titleTemplate.replace("%s", title) : titleTemplate.slice(4),
+          site_name: head?.og_site_name,
+          url: head?.og_url,
+          locale: head?.og_locale,
+          type: head?.og_type,
+          description: head?.og_description,
+          images: head?.og_image,
+          article: {
+            publishedTime: head?.article_published_time,
+            modifiedTime: head?.article_modified_time,
+          },
+        }}
+        twitter={{
+          cardType: head?.twitter_card,
+        }}
+        robotsProps={{
+          maxSnippet: Number(head?.robots["max-snippet"]),
+          maxVideoPreview: Number(head?.robots["max-video-preview"]),
+        }}
+        noindex={!(head?.robots.index ? head.robots.index === "index" : true)}
+        nofollow={!(head?.robots.follow ? head.robots.follow === "follow" : true)}
+        canonical={head?.canonical}
+      />
+    </div>
+  );
+});
+
+Layout.displayName = "Layout";
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+const meta = {
+  title: "Pożywka - kulturalnie o kulinariach",
+  description: "Cześć, tu Dominika! Jestem antropolożką i dziennikarką, opowiadam o jedzeniu.",
+};
+
+export const titleTemplate = `%s | ${meta.title}`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
