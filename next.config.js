ca027d04178d6249c875164f29626284ec6a85c4
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2022-08-22 13:35:58 +0200

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,20 +1,27 @@
 /** @type {import('next').NextConfig} */
 const withReactSvg = require("next-react-svg");
+const withPWA = require("next-pwa");
 const path = require("path");
 
-module.exports = withReactSvg({
-  include: path.resolve(__dirname, "public/svg"),
-  webpack(config, options) {
-    return config;
-  },
-  reactStrictMode: true,
-  images: {
-    domains: [
-      "scontent-waw1-1.cdninstagram.com",
-      "scontent-frt3-2.cdninstagram.com",
-      "scontent-frx5-1.cdninstagram.com",
-      "scontent-frt3-1.cdninstagram.com",
-      "admin.pozywka.pl",
-    ],
-  },
-});
+module.exports = withPWA(
+  withReactSvg({
+    include: path.resolve(__dirname, "public/svg"),
+    webpack(config, options) {
+      return config;
+    },
+    reactStrictMode: true,
+    images: {
+      domains: [
+        "scontent-waw1-1.cdninstagram.com",
+        "scontent-frt3-2.cdninstagram.com",
+        "scontent-frx5-1.cdninstagram.com",
+        "scontent-frt3-1.cdninstagram.com",
+        "admin.pozywka.pl",
+      ],
+    },
+    pwa: {
+      dest: "public",
+      disable: process.env.NODE_ENV === "development",
+    },
+  }),
+);
