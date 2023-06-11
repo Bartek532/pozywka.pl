0b00be919daf6fadd699ed9f27c08e1ebeaecffa
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2023-06-11 15:52:15 +0200

    chore: update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: ["plugin:react-hooks/recommended", "next", "next/core-web-vitals", "prettier"],
   plugins: ["react-hooks", "@typescript-eslint", "prettier"],
-
   env: {
     es6: true,
     browser: true,
